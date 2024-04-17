da3b23d558143edaa48311408c1387f6e6528854
LorenzGartmeier <l.gartmeier@gmail.com>
2024-04-17 19:05:04 +0200

    added impl for class Board

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,39 @@
 public class Board {
+
+    //gibt an, welcher Spieler den nächsten Zug machen darf
+    public boolean yellowToTurn;
+
+    //Speicherung der Steine und Felder
+    //der erste Index gibt die Zeile an, der zweite die Spalte
+    //Zeile 0 ist die unterste Zeile -> Aufpassen beim Drucken
+    public int[][] squares;
+
+    //mögliche Einträge in squares
+    public static final int YELLOW = 1;
+    public static final int RED = 2;
+    public static final int FREE = 0;
+
+    public Board(){
+        yellowToTurn = true;
+        squares = new int[6][7];
+    }
+
+    public void clear(){
+        for(int line = 0; line < 6; line++){
+            for(int column = 0; column < 7; column++) squares[line][column] = FREE;
+        }
+    }
+
+    public boolean columnIsFull(int column){
+        return squares[5][column] > 0;
+    }
+
+    public boolean isFull(){
+        //@TODO: wenn das ganze Brett voll ist
+        // Du schaffst das Leon, vllt kannst du sogar bereits vorhandenen Code wiederverwenden
+        return false;
+    }
+
+
+
 }
