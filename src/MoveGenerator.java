d238c403e1bf7f1f2029fb4f058ad27fc0202623
LorenzGartmeier <l.gartmeier@gmail.com>
2024-04-17 19:19:03 +0200

    created class MoveGenerator with implementation

--- src/MoveGenerator.java
+++ src/MoveGenerator.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class MoveGenerator {
+import jdk.internal.classfile.Signature;
+
+import java.util.ArrayList;
+
+public class MoveGenerator {
+
+
+    //gibt alle Spalten zurück, die nicht voll sind und man somit werfen darf
+    public static ArrayList<Integer> getLegalMoves(Board board){
+        ArrayList<Integer> legalMoves = new ArrayList<>();
+
+        for(int column = 0; column < Board.WIDHT; column++){
+            if(!board.columnIsFull(column)) legalMoves.add(column);
+        }
+        return legalMoves;
+    }
+
+
 }
